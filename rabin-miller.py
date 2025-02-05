dc5604410928bac65a01ea7216e05567b06d5e76
al8763be <al8763be-s@student.lu.se>
2025-02-05 18:12:52 +0100

    Question 3 done, added generation of primes depending on bits for question 4 aswell

--- rabin-miller.py
+++ rabin-miller.py
@@ -1,4 +1,5 @@
 import random
+import timeit
 
 
 def ProbablyPrime(n, k=20):  # k is the number of rounds
@@ -8,6 +9,29 @@ def ProbablyPrime(n, k=20):  # k is the number of rounds
         if not miller_rabin(n):
             return False  # composite
     return True  # primew
+
+def miller_rabin_iterative(n):
+    s = n - 1
+    r = 0
+
+    while s % 2 == 0:
+        s = s // 2
+        r += 1
+    
+    a = random.randint(2, n - 2)
+    x = pow(a,s,n)
+    if x == 1 or x == n - 1:
+        return True # n is prime
+    
+    for i in range(1, r): # r - 1 give 5 is composite
+        #x = pow(a, (2 ** i) * s, n)
+        x = (x * x) % n
+        if x == 1:
+            return False # n is composite
+        if x == n - 1:
+            return True # n is prime
+        
+    return False # n is composite
 
 def miller_rabin(n):
     s = n - 1
@@ -52,7 +76,7 @@ def Mtest():
 def Atest():
     print("Automatic test mode selected")
     
-    with open("prime2.txt", "r") as file:
+    with open("prime.txt", "r") as file:
         for line in file:
             parts = line.split()
             for part in parts:
@@ -65,19 +89,47 @@ def Atest():
                     print("Number is composite")
 
 
-def random_mode():
-    found = False
-    print("Random mode selected")
+def comp_test():
+    print ("Computational time test mode selected")
 
-    while not found:
-        n = random.getrandbits(512)
-        if(n % 2 != 0 and n > 3):
-            found = True
-            print("Random number generated: ", n)
-            if ProbablyPrime(n) == True:
-                print("Number is prime")
-            else:
-                print("Number is composite")
+
+    # number to test, currently the biggest one from primeXL
+    n = 15485863
+
+    #Number is how many times we want to run the test
+    iterative_time = timeit.timeit(lambda: miller_rabin_iterative(n), number=100000)
+    recompute_time = timeit.timeit(lambda: miller_rabin(n), number=100000)
+
+   
+    print("Iterative squaring average time:", iterative_time / 100000, "seconds per run")
+    print("Recomputing exponent average time:", recompute_time / 100000, "seconds per run")
+    print("Time difference:", (iterative_time / 100000) - (recompute_time / 100000), "seconds")
+    
+    print("\n")
+
+def generate_with_time():
+    print("Generate primes seleceted, pick bits: 512, 1024, 2048, 4096")
+    
+    bits = int(input())
+    
+
+    start = timeit.default_timer()
+    generate(bits)
+    end = timeit.default_timer()
+    print("Time taken to generate 100 primes:", end - start, "seconds")
+    print("\n")
+
+def generate(bits):
+    count = 0
+    primes = []
+    while count < 100:
+        
+        n = (random.getrandbits(bits) | (1 << bits)) | 1
+        if ProbablyPrime(n):
+            count += 1
+            primes.append(n)
+    return primes
+
     
    
 
@@ -86,9 +138,10 @@ def main():
 
     while not end:
         print("For manual test mode press 1,\n"
-              "for automatic test mode press 2,\n"
-              "for random mode press 3,\n"
-              "to exit press q: ")
+            "For automatic test mode press 2,\n"
+            "For computational time test press 3,\n"
+            "For generating primes press 4,\n"
+            "To exit press q: ")
         
         pick = input()
         if pick == "1":
@@ -96,7 +149,9 @@ def main():
         elif pick == "2":
             Atest()
         elif pick == "3":
-            random_mode()
+            comp_test()
+        elif pick == "4":
+            generate_with_time()
         elif pick == "q":
             end = True
         else:
